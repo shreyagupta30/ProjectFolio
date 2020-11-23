b25313590222fa76bbbf930c43fb3d02a33e73ea
shreyagupta30 <shreyagupta3011@gmail.com>
2020-11-23 17:44:01 +0530

    change svg

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,6 @@ const Footer = () => {
         <footer className="text-gray-500 bg-gray-900 body-font">
   <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
     <a className="flex title-font font-medium items-center md:justify-start justify-center text-white">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24" style={{darkreaderInlineFill:"none",  darkreaderInlineStroke:"currentColor"}} data-darkreader-inline-fill="" data-darkreader-inline-stroke="">
-        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-      </svg>
       <span className="ml-3 text-xl">MLH Fellowship</span>
     </a>
     <p className="text-sm text-gray-600 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4">2020 © Major League Hacking PBC Inc.
